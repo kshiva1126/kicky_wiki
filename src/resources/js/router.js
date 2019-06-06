f0500e6677393ebbbc9538d4fe20cbfe8cbdcf14
Yusuke Ito <maroemon58@gmail.com>
2019-06-06 15:59:19 +0900

    投稿画面追加 #1

--- src/resources/js/router.js
+++ src/resources/js/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 
 import Top from './pages/TopComponent.vue';
+import Post from './pages/PostComponent.vue';
 
 Vue.use(VueRouter);
 
@@ -9,6 +10,10 @@ const routes = [
     {
         path: '/',
         component: Top
+    },
+    {
+        path: '/post',
+        component: Post
     }
 ];
 
