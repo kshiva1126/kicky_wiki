f6a11ebbc24f916e37b7527c3193c60217bcb898
Yusuke Ito <maroemon58@gmail.com>
2019-06-06 16:08:27 +0900

    ナビバー追加 #1

--- src/resources/js/App.vue
+++ src/resources/js/App.vue
@@ -1,7 +1,20 @@
 <template>
   <div>
+    <header>
+      <Navbar/>
+    </header>
     <main>
       <RouterView/>
     </main>
   </div>
 </template>
+
+<script>
+import Navbar from "./components/Navbar";
+
+export default {
+  components: {
+    Navbar
+  }
+};
+</script>
---
+++ src/resources/js/components/Navbar.vue
@@ -0,0 +1,7 @@
+<template>
+  <p>Navbar</p>
+</template>
+
+<script>
+export default {};
+</script>
