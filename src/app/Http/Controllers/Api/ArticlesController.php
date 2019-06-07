f9602acb4e4b60972bd197a556745bcbbe56faf2
kshiva1126 <42695879+kshiva1126@users.noreply.github.com>
2019-06-07 18:31:44 +0900

    本文のHTMLを除外するように修正 #12

--- src/app/Http/Controllers/Api/ArticlesController.php
+++ src/app/Http/Controllers/Api/ArticlesController.php
@@ -20,7 +20,7 @@ class ArticlesController extends Controller
         $data = [];
         foreach ($articles as $idx => $article) {
             $data[$idx]['title'] = $article->title;
-            $data[$idx]['body'] = $article->short_mark_body;
+            $data[$idx]['body'] = $article->short_body;
             // 一覧画面には必要ない情報かも？
             foreach ($article->article_images as $image) {
                 $data[$idx]['image'][] = $image->image_path;
