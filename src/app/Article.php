d7ff41be4b0f5038b56e2a26029e80d3cb3876ec
kshiva1126 <42695879+kshiva1126@users.noreply.github.com>
2019-06-09 10:40:06 +0900

    developブランチとの差分解消

--- src/app/Article.php
+++ src/app/Article.php
@@ -9,15 +9,6 @@ use cebe\markdown\GithubMarkdown as Markdown;
 class Article extends Model
 {
     /**
-<<<<<<< HEAD
-     * 一覧表示用に内容を100文字で丸め、Markdownにparseする
-     */
-    public function getShortMarkBodyAttribute()
-    {
-        // "..."も含めて100文字で丸める
-        $short_body = mb_strimwidth($this->body, 0, 100, '...', 'UTF-8');
-        return $this->parse($short_body);
-=======
      * 一覧表示用に内容を100文字で丸め、Markdownにparseした上で
      * HTML および PHP タグを取り除いた文字列を返す
      */
@@ -31,7 +22,6 @@ class Article extends Model
         $short_body = strip_tags($this->parse($short_body));
         // 改行コードを取り除く
         return str_replace(['\r', '\n'], $short_body, '');
->>>>>>> 5422dc375f52e1969898d366614ed5b9543ef2ea
     }
 
     /**
--- src/app/Http/Controllers/Api/ArticlesController.php
+++ src/app/Http/Controllers/Api/ArticlesController.php
@@ -2,13 +2,8 @@
 
 namespace App\Http\Controllers\Api;
 
-<<<<<<< HEAD
 use App\Article;
 use App\Http\Requests\ArticleRequest;
-=======
-use Illuminate\Http\Request;
-use App\Article;
->>>>>>> 5422dc375f52e1969898d366614ed5b9543ef2ea
 use Illuminate\Support\Facades\Log;
 use App\Http\Controllers\Controller;
 
@@ -24,14 +19,9 @@ class ArticlesController extends Controller
         $articles = Article::all();
         $data = [];
         foreach ($articles as $idx => $article) {
-<<<<<<< HEAD
-            $data[$idx]['title'] = $article->title;
-            $data[$idx]['body'] = $article->short_mark_body;
-=======
             $data[$idx]['id'] = $article->id;
             $data[$idx]['title'] = $article->title;
             $data[$idx]['body'] = $article->short_body;
->>>>>>> 5422dc375f52e1969898d366614ed5b9543ef2ea
             // 一覧画面には必要ない情報かも？
             foreach ($article->article_images as $image) {
                 $data[$idx]['image'][] = $image->image_path;
@@ -43,7 +33,6 @@ class ArticlesController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-<<<<<<< HEAD
      * @param  App\Http\Requests\ArticleRequest $request
      * @return \Illuminate\Http\Response
      */
@@ -54,14 +43,6 @@ class ArticlesController extends Controller
         $article->body = $request->body;
         $article->save();
         return redirect('api/articles');
-=======
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
->>>>>>> 5422dc375f52e1969898d366614ed5b9543ef2ea
     }
 
     /**
@@ -78,19 +59,11 @@ class ArticlesController extends Controller
     /**
      * Update the specified resource in storage.
      *
-<<<<<<< HEAD
      * @param  App\Http\Requests\ArticleRequest $request
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
     public function update(ArticleRequest $request, $id)
-=======
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
->>>>>>> 5422dc375f52e1969898d366614ed5b9543ef2ea
     {
         //
     }
