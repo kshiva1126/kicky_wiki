33338b9c63a3649890d369e1b7e8ee3c1522a29a
Yusuke Ito <maroemon58@gmail.com>
2019-06-15 15:59:42 +0900

    Revert "CKEditor組み込み #17"
    This reverts commit b7d6ae0288567b987293997e70fc27ae5cfce02a.

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -3,14 +3,11 @@ require('./bootstrap');
 import Vue from 'vue';
 import router from './router';
 
-import CKEditor from '@ckeditor/ckeditor5-vue';
-Vue.use(CKEditor);
-
 import App from './App.vue';
 
 const app = new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App />'
+    el: '#app',
+    router,
+    components: { App },
+    template: '<App />'
 });
--- src/resources/js/pages/PostComponent.vue
+++ src/resources/js/pages/PostComponent.vue
@@ -1,52 +1,7 @@
 <template>
-  <div class="container">
-    <form class="article__form">
-      <div class="article__title">
-        <input type="text" name id>
-      </div>
-      <div class="article__body">
-        <ckeditor :editor="editor" v-model="article.body" :config="editorConfig"></ckeditor>
-      </div>
-      <div class="article__button"></div>
-    </form>
-  </div>
+  <h1>Post Page</h1>
 </template>
 
 <script>
-import ClassicEditor from "@ckeditor/ckeditor5-editor-classic/src/classiceditor";
-
-import Autoformat from "@ckeditor/ckeditor5-autoformat/src/autoformat";
-import EssentialsPlugin from "@ckeditor/ckeditor5-essentials/src/essentials";
-import BoldPlugin from "@ckeditor/ckeditor5-basic-styles/src/bold";
-import ItalicPlugin from "@ckeditor/ckeditor5-basic-styles/src/italic";
-import ParagraphPlugin from "@ckeditor/ckeditor5-paragraph/src/paragraph";
-import LinkPlugin from "@ckeditor/ckeditor5-link/src/link";
-import GFMDataProcessor from "@ckeditor/ckeditor5-markdown-gfm/src/gfmdataprocessor";
-
-export default {
-  data() {
-    return {
-      article: {
-        title: "",
-        body: ""
-      },
-      editor: ClassicEditor,
-      editorConfig: {
-        // The configuration of the editor.
-        plugins: [
-          Autoformat,
-          EssentialsPlugin,
-          BoldPlugin,
-          ItalicPlugin,
-          LinkPlugin,
-          ParagraphPlugin,
-          GFMDataProcessor
-        ],
-        toolbar: {
-          items: ["bold", "italic", "link", "undo", "redo"]
-        }
-      }
-    };
-  }
-};
+export default {};
 </script>
