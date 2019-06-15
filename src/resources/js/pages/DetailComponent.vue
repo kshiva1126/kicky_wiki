304bdb604459206b6c34e5fb137130dea0d61acc
Yusuke Ito <maroemon58@gmail.com>
2019-06-15 17:28:18 +0900

    記事詳細情報を取得・表示 #24

--- src/resources/js/pages/DetailComponent.vue
+++ src/resources/js/pages/DetailComponent.vue
@@ -1,7 +1,25 @@
 <template>
-  <h1>Detail Page</h1>
+  <div class="container">
+    <div class="article">
+      <div class="article__title">{{ article.title }}</div>
+      <div class="article__body">{{ article.body }}</div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      article: ""
+    };
+  },
+  created() {
+    const response = axios
+      .get(`/api/articles/${this.$route.params.id}`)
+      .then(res => {
+        this.article = res.data;
+      });
+  }
+};
 </script>
